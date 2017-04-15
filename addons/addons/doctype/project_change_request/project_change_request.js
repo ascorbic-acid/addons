3561205648e294fee2ad1f9bf328b0be203070dc
Imad Abdou <igentle.appletec@gmail.com>
2017-04-15 15:54:30 +0200

    project change request done

--- addons/addons/doctype/project_change_request/project_change_request.js
+++ addons/addons/doctype/project_change_request/project_change_request.js
@@ -6,5 +6,16 @@ frappe.ui.form.on('Project Change Request', {
 
 	},
 	project: function(frm, cdt, cdn) {
+		if(frm.doc.project){
+			frappe.call({
+				method: 'addons.api.get_proj_info',
+				args: {
+					project: frm.doc.project
+				},
+				callback: function(data){
+					frappe.model.set_value(cdt, cdn, "project_name", data.message.project_name, "Link");
+				}
+			});
+		}
 	}
 });
--- addons/api.py
+++ addons/api.py
@@ -5,3 +5,8 @@ def get_emp_info(employee):
     emp_info = frappe.get_doc("Employee", employee)
 
     return emp_info
+@frappe.whitelist()
+def get_proj_info(project):
+    proj = frappe.get_doc("Project", project)
+
+    return proj
